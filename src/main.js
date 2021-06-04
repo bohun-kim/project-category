8e8111812b551d6863eb41b2777ed9a0f3c82223
bohun-kim <rlaqhgns0410@naver.com>
2021-06-04 23:36:14 +0900

    project-menu

--- src/main.js
+++ src/main.js
@@ -24,27 +24,51 @@ function loadItems(){
     }
 
     //카테고리 변수 설정                           
-    const dropdown = document.querySelector(".dropdown"); //div
-    const categoryContainer = document.querySelectorAll(".select_categories") //ul
-    const toggleBtn = document.querySelector(".select_dropdown") //button
-    
-    toggleBtn.addEventListener('click',function(){
-        categoryContainer.classList.toggle('show')
+    const drop = document.querySelector(".dropdown"); //div
+    const categoryContainer = document.querySelector(".select_categories"); //ul
+    const toggleBtn = document.querySelector(".select_dropdown"); //button
+    const selectBtn = document.querySelector(".selet-btn"); //ul>li>button
+
+    drop.addEventListener("mousedown", (e) => {
+        const target = e.target
+        if(target.classList.contains("select_dropdown")){
+            dropMenu();
+        } else if(target.classList.contains("selet-btn")){
+            selectMenu(target);
+        }
+    })
+
+    //드롭다운
+    function dropMenu(){
+        categoryContainer.classList.toggle("show");
+    }
+
+    //선택한 운동 버튼에 보이기
+    function selectMenu(category){
+        toggleBtn.innerHTML = category.innerHTML;
+        toggleBtn.classList.add('selected')
+    }
+
+    //블러효과
+    toggleBtn.addEventListener('blur', () => {
+         categoryContainer.classList.remove('show')
     })
 
     //클릭시 리스트 이벤트
     function setEventListeners(exercise){
         const logo = document.querySelector('.main-container_title');
+        
         logo.addEventListener('click', () => displayExercise(exercise));
-        button.addEventListener('click', event => onButtonClick(event, exercise));
+        drop.addEventListener('click', event => onButtonClick(event, exercise));
     }
 
+    
     // 필터링 효과
     function onButtonClick(event,exercise){
         const dataset = event.target.dataset //버튼을 눌렀을 때 type 설정하면 hand
         const key = dataset.key; 
         const value = dataset.value;
-    
+        console.log(value)
         const filtered = exercise.filter( x => x[key] === value);
         displayExercise(filtered)             //x[key] = "type : foot" === value = html 안에 있는 data-value
     }                                         //아이템 오브젝트안에  key에 해당하는 값이 원하는 value와 똑같은 아이들만 전달
@@ -54,5 +78,5 @@ function loadItems(){
 loadItems()
 .then(exercise => {
     displayExercise(exercise)       //전달받은 것을 나타내주는 함수
-    //setEventListeners(exercise)    //받아온 exercise들을 버튼을 누르면 필터링을 추가하는 함수
+    setEventListeners(exercise)    //받아온 exercise들을 버튼을 누르면 필터링을 추가하는 함수
 })
